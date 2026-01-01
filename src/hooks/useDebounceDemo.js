95ac7720dae960bf5e7a756122c3ee3f99447a16
Navaneeth <mnavaneeth321@gmail.com>
2026-01-01 14:48:07 +0530

    Add useDebounce custom hook

---
+++ src/hooks/useDebounceDemo.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+export default function useDebounce(value, delay) {
+  const [d, setD] = useState(value);
+  useEffect(() => {
+    const id = setTimeout(() => setD(value), delay);
+    return () => clearTimeout(id);
+  }, [value]);
+  return d;
+}
