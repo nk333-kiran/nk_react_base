ef3f468e027ae16c3623272b359f22510b8bb3b6
Navaneeth <mnavaneeth321@gmail.com>
2026-01-01 14:48:53 +0530

    Add usePrevious custom hook for value tracking

---
+++ src/hooks/usePrevious.js
@@ -0,0 +1,8 @@
+import { useEffect, useRef } from "react";
+export default function usePrevious(val) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = val;
+  });
+  return ref.current;
+}
